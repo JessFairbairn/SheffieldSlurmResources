82d26efc7fc509d2d176df3e6c476c65f24e1a80
Jess Fairbairn <jessfairbairn91@gmail.com>
2024-07-08 16:14:21 +0100

    Added time limits to sessions and more explanations

--- .aliases.sh
+++ .aliases.sh
@@ -1,10 +1,16 @@
-# My 1337 Slurm scripts, I run this as part of my .bashrc startup file so they're always available
+# My leet Slurm scripts, I run this as part of my .bashrc startup file so they're always available
 # These assume everything is getting saved in the format "logs/output-<jobcode>"
 
-alias myjobs='sacct -u lip21jaf -X -S2023-09-15-10:30 -o JobID,JobName,State,Start,Elapsed'
+# This one just displays your job history. Quick way to see the state of your latest job.
+alias myjobs='sacct -u lip21jaf -X -S2024-06-01-00:00 -o JobID,JobName,State,Start,Elapsed'
+
 alias runningjobid='sacct -u lip21jaf --noheader -s r -o JobID -X'
-alias tailjob='tail -f $(ls logs/*$(lastjobid))'
 alias lastjobid='sacct -u lip21jaf --noheader -o JobID,JobName -X -P | grep -v "bash" | tail -1 |cut -d "|" -f 1'
+
+# Display the logs of the currently running job as it runs:
+alias tailjob='tail -f $(ls logs/*$(lastjobid))'
 alias lastlogs='less $(ls logs/*$(lastjobid))'
 
-# I run this script as part of .bashrc
+# I can never remember these. Time limit to avoid hogging resources (or being penalised for overuse)
+alias int_session='srun --mem=16G --pty bash -i --time=2:00:00'
+alias gpu_session='srun --mem=16G --qos=gpu --partition=gpu --gpus-per-node=1 --pty bash -i --time=2:00:00'
