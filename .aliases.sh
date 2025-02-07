1872d925293b5dbfffbb991860c6d1a3ffc5c864
Jess <jessfairbairn91@gmail.com>
2025-02-07 16:32:49 +0000

    Lots of code fixes
    Removed hardcoded username (d'oh)
    Fixed filtering on lastjobid
    Expanded width of job name on myjobs as the default is very narrow
    Clarified some instructions

--- .aliases.sh
+++ .aliases.sh
@@ -1,16 +1,20 @@
 # My leet Slurm scripts, I run this as part of my .bashrc startup file so they're always available
-# These assume everything is getting saved in the format "logs/output-<jobcode>"
 
-# This one just displays your job history. Quick way to see the state of your latest job.
-alias myjobs='sacct -u lip21jaf -X -S2024-06-01-00:00 -o JobID,JobName,State,Start,Elapsed'
+# This one just displays your job history. Quick way to see the state of your latest job. 
+alias myjobs='sacct -u $(whoami) -X -S2024-06-01-00:00 -o JobID,JobName%20,State,Start,Elapsed'
 
-alias runningjobid='sacct -u lip21jaf --noheader -s r -o JobID -X'
-alias lastjobid='sacct -u lip21jaf --noheader -o JobID,JobName -X -P | grep -v "bash" | tail -1 |cut -d "|" -f 1'
+alias runningjobid='sacct -u $(whoami) --noheader -s r -o JobID -X'
 
-# Display the logs of the currently running job as it runs:
+# This one will generally pick up the last one. All that grepping is so it only picks up batch jobs.
+alias lastjobid='sacct -u $(whoami) --noheader -o JobID,JobName -X -P | grep -v -e "bash" -e "\[" | tail -1 |cut -d "|" -f 1'
+
+# Display the logs of the currently running job as it runs
+# These assume job output is getting saved with filename "~/logs/<some-string>-<jobcode>"
 alias tailjob='tail -f $(ls ~/logs/*$(lastjobid))'
 alias lastjobout='less $(ls ~/logs/*$(lastjobid))'
 
 # I can never remember these. Time limit to avoid hogging resources (or being penalised for overuse)
 alias int_session='srun --mem=16G --time=2:00:00 --pty bash -i'
+
+# if you need to use this one, you're having a bad day.
 alias gpu_session='srun --mem=16G --qos=gpu --partition=gpu --gpus-per-node=1 --time=2:00:00 --pty bash -i'
